cb9b4705cb9db7ece6d50c346e69dabc2cec953b
LeeVaughn <dlvprogramming@gmail.com>
2019-02-07 20:52:09 -0800

    required json data and created new routes

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const data = require("./data.json");
+const projects = data.projects;
 
-// creates root route
+app.set("view engine", "pug");
+
+// GET root route
 app.get("/", (req, res) => {
-  res.send("test");
+  res.render("layout", projects);
+});
+
+// GET about route
+app.get("/about", (req, res) => {
+  res.render("about", projects);
+});
+
+// GET about route
+app.get("/project:id", (req, res) => {
+  res.render("about", projects);
 });
 
 app.listen(3000, () => {
