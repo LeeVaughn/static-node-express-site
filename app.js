a7ac14963ce4e08d3eadc9ed9b7c32ba2686d382
LeeVaughn <dlvprogramming@gmail.com>
2021-01-26 15:52:37 -0800

    Update error handling

--- app.js
+++ app.js
@@ -7,18 +7,25 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 app.use(routes);
 
-// creates error for non-existent routes and triggers error handler
+// 404 handler to catch undefined or non-existent route requests
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+  console.log('404 error handler called');
+
+  res.status(404).render("not-found");
 });
 
-// error handler
+// Global error handler
 app.use((err, req, res, next) => {
-  res.locals.error = err;
-  console.log(`Oops! There has been a ${err.status}, ${err.message} error`)
-  res.render("error");
+  if (err) {
+    console.log('Global error handler called', err);
+  }
+  
+  if (err.status === 404) {
+    res.status(404).render("not-found", { err });
+  } else {
+    err.message = err.message || "Oops! It looks like something went wrong on the server.";
+    res.status(err.status || 500).render("error", { err });
+  }
 });
 
 app.listen(3000, () => {
